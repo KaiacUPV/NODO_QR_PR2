5ce5ce630dfb26bd906ab5693d04e6f4c55eab6a
KaiacUPV <kaoican@upv.edu.es>
2025-05-10 01:05:10 +0200

    Arreglillo

--- btns_qr_cam_mqtt/w_loop.ino
+++ btns_qr_cam_mqtt/w_loop.ino
@@ -9,17 +9,20 @@ void on_loop() {
     Serial.println("Botón 1 presionado");
     acc = "Iinicio de la simulación";
     enviarMensajePorTopic(CAM_TOPIC, acc, pers);
+    enviarMensajePorTopic(RDK_TOPIC, acc, pers);
 
   }
   if (digitalRead(BOTON2) == LOW) {
     Serial.println("Botón 2 presionado");
     acc = "Parada de la simulación";
     enviarMensajePorTopic(CAM_TOPIC, acc, pers);
+    enviarMensajePorTopic(RDK_TOPIC, acc, pers);
   }
   if (digitalRead(BOTON3) == LOW) {
     Serial.println("Botón 3 presionado");
     acc = "Parada y reseteada la simulación"; 
     enviarMensajePorTopic(CAM_TOPIC, acc, pers);
+    enviarMensajePorTopic(RDK_TOPIC, acc, pers);
   }
 
 
